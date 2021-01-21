f77a938236dcedb9009e775b79b422ff70f72b7f
sshindanai <sshindanai.m@gmail.com>
2021-01-21 16:11:05 +0700

    init commit

---
+++ oauth/oauth.go
@@ -0,0 +1,120 @@
+package oauth
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/mercadolibre/golang-restclient/rest"
+	"github.com/sshindanai/bookstore-oauth-go/oauth/errors"
+)
+
+var (
+	oauthRestClient = rest.RequestBuilder{
+		BaseURL: "http://localhost:1323",
+		Timeout: 100 * time.Millisecond,
+	}
+)
+
+const (
+	headerXPublic   = "X-Public"
+	headerXClientID = "X-Client-ID"
+	headerXCallerID = "X-Caller-ID"
+
+	paramAccessToken = "access_token"
+)
+
+type accessToken struct {
+	ID       string `json:"id"`
+	UserID   int64  `json:"user_id"`
+	ClientID int64  `json:"client_id"`
+}
+
+func ClientID(request *http.Request) int64 {
+	if request == nil {
+		return 0
+	}
+	clientID, err := strconv.ParseInt(request.Header.Get(headerXClientID), 10, 64)
+	if err != nil {
+		return 0
+	}
+	return clientID
+}
+
+func CallerID(request *http.Request) int64 {
+	if request == nil {
+		return 0
+	}
+
+	callerID, err := strconv.ParseInt(request.Header.Get(headerXCallerID), 10, 64)
+	if err != nil {
+		return 0
+	}
+	return callerID
+}
+
+func IsPublic(request *http.Request) bool {
+	if request == nil {
+		return true
+	}
+	return request.Header.Get(headerXPublic) == "true"
+}
+
+func AuthenticateRequest(request *http.Request) *errors.RestErr {
+	if request == nil {
+		return nil
+	}
+
+	cleanRequest(request)
+
+	accessTokenID := strings.TrimSpace(request.URL.Query().Get(paramAccessToken))
+	if accessToken == "" {
+		return nil
+	}
+
+	at, err := getAccessToken(accessTokenID)
+	if err != nil {
+		if err.Code == http.StatusNotFound {
+			return nil
+		}
+		return err
+	}
+
+	request.Header.Add(headerXClientID, fmt.Sprintf("%v", at.ClientID))
+	request.Header.Add(headerXCallerID, fmt.Sprintf("%v", at.UserID))
+
+	return nil
+}
+
+func cleanRequest(request *http.Request) {
+	if request == nil {
+		return
+	}
+
+	request.Header.Del(headerXClientID)
+	request.Header.Del(headerXCallerID)
+}
+
+func getAccessToken(at string) (*accessToken, *errors.RestErr) {
+	response := oauthRestClient.Get(fmt.Sprintf("/oauth/access_token/%s", at))
+	if response == nil || response.Response == nil {
+		return nil, errors.NewNotFoundError("invalid restclient response when trying to get access token")
+	}
+
+	if response.StatusCode > 299 {
+		var restErr errors.RestErr
+		if err := json.Unmarshal(response.Bytes(), &restErr); err != nil {
+			return nil, errors.NewNotFoundError("invalid error interface when trying to get access token")
+		}
+		return nil, &restErr
+	}
+
+	var token accessToken
+	if err := json.Unmarshal(response.Bytes(), &token); err != nil {
+		return nil, errors.NewNotFoundError("error when trying to unmarshal access token")
+	}
+	return &token, nil
+}
